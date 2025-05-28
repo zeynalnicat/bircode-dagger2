19190d8738e35a675bc69815d3c1b4cd148bf37c
Nicat Zeynalli <nicat21308@gmail.com>
2025-05-28 20:45:11 +0400

    handling device navigation back

--- home/src/main/java/com/example/dagger2_app/ui/fragments/home/HomeFragment.kt
+++ home/src/main/java/com/example/dagger2_app/ui/fragments/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 
@@ -48,9 +49,18 @@ class HomeFragment : Fragment(), NotesAdapter.ICallback {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         (requireActivity().application as Injection).inject(this)
         setNavigation()
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    router.finishChain()
+                }
+            }
+        )
 
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
